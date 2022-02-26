592df24d3bc5402a308c6c4d6dd4b674e5510232
clarencelol <clarencekuiek@icloud.com>
2022-02-26 18:59:43 +0800

    wayne-common: Add Gcam GO
    * This is the best camera for osscam
    * Based on Greatness's CameraGo
    
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>

--- wayne-common.mk
+++ wayne-common.mk
@@ -30,6 +30,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.6 \
     camera.sdm660 \
     libmm-qcamera \
+    GcamGo \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0.vendor
 
